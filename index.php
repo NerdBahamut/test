41d9bf8564f84d2574a9a2763fa786c7fc49e619
NerdBahamut <34350408+NerdBahamut@users.noreply.github.com>
2018-02-10 15:08:43 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Тестовая работа</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body class="body">
+
+    <header class="header">
+		<div class="w_1024">
+			<nav class="nav_head">
+				<ul>
+					<li><a href="#" class="click_left_block">Left block</a></li>
+					<li><a href="#" class="click_right_block">Right block</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	
+	<div class="wrapper w_1024">
+	
+		<div class="content">
+			
+			<div class="page_description"></div>
+			
+			<div id="sticky_block" class="sticky_block">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in suscipit nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit mollis posuere. Pellentesque sit amet pulvinar neque. Sed gravida arcu eu nisl faucibus placerat. Etiam dapibus scelerisque tortor, et lacinia magna dignissim at. Mauris a volutpat nisl, in semper ipsum. Maecenas quis faucibus orci. Sed rutrum fringilla maximus. Vivamus consectetur ipsum lectus, ac tincidunt orci congue at.
+
+				Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam et dictum tortor. Nunc quis accumsan arcu. Suspendisse ut interdum est. Mauris elementum justo in urna porttitor, nec semper mi convallis. Nulla sed magna volutpat, dapibus velit quis, venenatis leo. Fusce eu quam sit amet dui scelerisque condimentum. Quisque iaculis ornare mauris, sed suscipit dolor fringilla a.
+			</div>
+			
+		</div>
+		
+		<div class="left_aside"></div>
+		<div class="right_aside"></div>
+	
+		<footer class="footer">Footer</footer>
+	</div>
+	
+    <script src="/js/jquery.js"></script>
+    <script src="/js/default.js"></script>
+</body>
+</html>
