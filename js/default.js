e8bf2ddff700db50e0be9ebd80986def98085441
NerdBahamut <34350408+NerdBahamut@users.noreply.github.com>
2018-02-10 15:13:28 +0600

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	
+	$('.nav_head a').click(function(){
+		if($(this).hasClass('click_left_block')){
+			if(!$('.left_aside').hasClass('active')){
+				$('.left_aside').addClass('active');
+			} else {
+				$('.left_aside').removeClass('active');
+			}
+		
+			return false;
+		}
+		
+		if($(this).hasClass('click_right_block')){
+			if(!$('.right_aside').hasClass('active')){
+				$('.right_aside').addClass('active');
+			} else {
+				$('.right_aside').removeClass('active');
+			}
+			
+			return false;
+		}
+	});
+	
+});
+
+(function(){
+	var a = document.querySelector('#sticky_block'), b = null, P = 40;
+	window.addEventListener('scroll', Ascroll, false);
+	document.body.addEventListener('scroll', Ascroll, false);
+	function Ascroll() {
+		if (b == null) {
+			var Sa = getComputedStyle(a, ''), s = '';
+			for (var i = 0; i < Sa.length; i++) {
+				if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+					s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+				}
+			}
+			b = document.createElement('div');
+			b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+			a.insertBefore(b, a.firstChild);
+			var l = a.childNodes.length;
+			for (var i = 1; i < l; i++) {
+				b.appendChild(a.childNodes[1]);
+			}
+			a.style.height = b.getBoundingClientRect().height + 'px';
+			a.style.padding = '0';
+			a.style.border = '0';
+		}
+		var Ra = a.getBoundingClientRect(), R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('footer').getBoundingClientRect().top + 0);
+		if ((Ra.top - P) <= 0) {
+			if ((Ra.top - P) <= R) {
+				b.className = 'stop';
+				b.style.top = - R +'px';
+			} else {
+				b.className = 'sticky';
+				b.style.top = P + 'px';
+			}
+		} else {
+			b.className = '';
+			b.style.top = '';
+		}
+		window.addEventListener('resize', function() {
+			a.children[0].style.width = getComputedStyle(a, '').width
+		}, false);
+	}
+})()
